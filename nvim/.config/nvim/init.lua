dd756e474bf5e8a5c03a1c23d26e7b45e3c1ec18
Tanner Kilian <tkilian12@gmail.com>
2025-05-29 14:14:14 -0700

    Fixed up lualine config to fit in opts

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,8 +1,5 @@
 require("config.lazy")
 
--- vim.o.background = "dark" -- or "light" for light mode
--- vim.cmd.colorscheme("gruvbox")
-
 vim.opt.number = true
 vim.opt.relativenumber = true
 
@@ -13,3 +10,5 @@ vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
+
+vim.opt.clipboard = "unnamedplus"
--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -2,6 +2,7 @@ return {
   "ellisonleao/gruvbox.nvim",
   priority = 1000,
   config = function()
+    -- vim.opt.background = "dark" -- or "light" for light mode
     vim.cmd.colorscheme("gruvbox")
   end,
 }
