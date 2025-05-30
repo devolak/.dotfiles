45fe85206933f1cda41e2411adad68fbcb36b09b
Tanner Kilian <tkilian12@gmail.com>
2025-05-29 21:06:02 -0700

    Added a simple telescope keybind to leader fd

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,5 +1,11 @@
 return {
   "nvim-telescope/telescope.nvim",
   branch = "0.1.x",
-  dependencies = { "nvim-lua/plenary.nvim" },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+  },
+  config = function()
+    vim.keymap.set("n", "<space>fd", require("telescope.builtin").find_files)
+  end
 }
