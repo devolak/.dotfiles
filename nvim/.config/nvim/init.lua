a393ba185716bd1bbef02c0afb082bd9ca3d68cd
Tanner Kilian <tkilian12@gmail.com>
2025-06-03 11:55:49 -0700

    Organized options into seperate file

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,14 +1,2 @@
+require("config.options")
 require("config.lazy")
-
-vim.opt.number = true
-vim.opt.relativenumber = true
-
-vim.opt.showmode = false
-
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-vim.opt.smartindent = true
-
-vim.opt.clipboard = "unnamedplus"
---
+++ nvim/.config/nvim/lua/config/options.lua
@@ -0,0 +1,15 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.showmode = false
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+vim.opt.clipboard = "unnamedplus"
