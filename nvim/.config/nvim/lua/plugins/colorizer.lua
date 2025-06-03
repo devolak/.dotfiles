a8987aefdc37499df981fa0edd2a65e2d61a9494
Tanner Kilian <tkilian12@gmail.com>
2025-06-03 11:55:13 -0700

    Switched colorizer to one being maintained

--- nvim/.config/nvim/lua/plugins/colorizer.lua
+++ nvim/.config/nvim/lua/plugins/colorizer.lua
@@ -1,12 +1,17 @@
 return {
-  "norcalli/nvim-colorizer.lua",
-  opts = { css = true, },
-  config = function()
-    require 'colorizer'.setup {
-      '*'; -- Highlight all files, but customize some others.
-      filetype = {
-        "hyprlang",
-      },
-    }
-  end,
+  "catgoose/nvim-colorizer.lua",
+  opts = {
+    user_defualt_options = {
+      RGB = true, -- #RGB hex codes
+      RGBA = true, -- #RGBA hex codes
+      RRGGBB = true, -- #RRGGBB hex codes
+      RRGGBBAA = true, -- #RRGGBBAA hex codes
+      AARRGGBB = true, -- 0xAARRGGBB hex codes
+      rgb_fn = true, -- CSS rgb() and rgba() functions
+      hsl_fn = true, -- CSS hsl() and hsla() functions
+      css = true, -- Enable all CSS *features*:
+      -- names, RGB, RGBA, RRGGBB, RRGGBBAA, AARRGGBB, rgb_fn, hsl_fn
+      css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+    },
+  },
 }
