ade335f1e135967e2998ea268cb92fc0873967d8
Tanner Kilian <tkilian12@gmail.com>
2025-06-05 21:51:57 -0700

    Fixed up colorizer settings

--- nvim/.config/nvim/lua/plugins/colorizer.lua
+++ nvim/.config/nvim/lua/plugins/colorizer.lua
@@ -1,15 +1,14 @@
 return {
   "catgoose/nvim-colorizer.lua",
   opts = {
-    user_defualt_options = {
+    user_default_options = {
+      names = false,
       RGB = true, -- #RGB hex codes
       RGBA = true, -- #RGBA hex codes
       RRGGBB = true, -- #RRGGBB hex codes
       RRGGBBAA = true, -- #RRGGBBAA hex codes
       AARRGGBB = true, -- 0xAARRGGBB hex codes
-      rgb_fn = true, -- CSS rgb() and rgba() functions
-      hsl_fn = true, -- CSS hsl() and hsla() functions
-      css = true, -- Enable all CSS *features*:
+      -- css = true, -- Enable all CSS *features*:
       -- names, RGB, RGBA, RRGGBB, RRGGBBAA, AARRGGBB, rgb_fn, hsl_fn
       css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
     },
