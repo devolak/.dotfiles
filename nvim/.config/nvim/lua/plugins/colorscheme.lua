afce73cb796bd0583de6ecd9090896bc714a5519
Tanner Kilian <tkilian12@gmail.com>
2025-06-05 21:52:24 -0700

    colorscheme changes

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,11 @@
 return {
-  "ellisonleao/gruvbox.nvim",
+  "sainnhe/gruvbox-material",
   priority = 1000,
   config = function()
     -- vim.opt.background = "dark" -- or "light" for light mode
-    vim.cmd.colorscheme("gruvbox")
+    -- vim.cmd.colorscheme("gruvbox")
+    vim.g.gruvbox_material_enable_italic = true
+    vim.g.gruvbox_material_foreground = "material"
+    vim.cmd.colorscheme("gruvbox-material")
   end,
 }
