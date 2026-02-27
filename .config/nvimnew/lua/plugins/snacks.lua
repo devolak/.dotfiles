c76ba0d25608aaf36fcc0ce838a1c5658ed7331e
Tanner Kilian <tkilian12@gmail.com>
2026-02-27 01:17:21 -0700

    Added snacks.nvim animate plugin

--- .config/nvimnew/lua/plugins/snacks.lua
+++ .config/nvimnew/lua/plugins/snacks.lua
@@ -7,6 +7,7 @@ return {
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
+    animate = { enabled = true },
     bigfile = { enabled = true },
     dashboard = {
       enabled = true,
