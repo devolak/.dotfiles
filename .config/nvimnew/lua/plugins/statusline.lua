2dda64efede6f272bc5c4a6dbc1f5a713efedbc9
Tanner Kilian <tkilian12@gmail.com>
2026-02-27 02:24:04 -0700

    Added last input cmd into lualine

--- .config/nvimnew/lua/plugins/statusline.lua
+++ .config/nvimnew/lua/plugins/statusline.lua
@@ -1,21 +1,29 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	opts = {
-		options = {
-			icons_enabled = false,
-			theme = "auto",
-			component_separators = { left = "|", right = "|" },
-			section_separators = { left = "", right = "" },
-		},
-		sectiopns = {
-			lualine_a = { "mode" },
-			lualine_b = { "branch", "diff", "diagnostics" },
-			lualine_c = { { "filename", path = 1, shorting_target = 40 } },
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {
+    options = {
+      icons_enabled = false,
+      theme = "auto",
+      component_separators = { left = "|", right = "|" },
+      section_separators = { left = "", right = "" },
+    },
+    sections = {
+      lualine_a = { "mode" },
+      lualine_b = { "branch", "diff", "diagnostics" },
+      lualine_c = { { "filename", path = 1, shorting_target = 40 } },
 
-			lualine_x = { "encoding", "fileformat", "filetype" },
-			lualine_y = { "progress" },
-			lualine_z = { "location" },
-		},
-	},
+      lualine_x = {
+        {
+          require("noice").api.status.command.get,
+          cond = require("noice").api.status.command.has,
+        },
+        "encoding",
+        "fileformat",
+        "filetype"
+      },
+      lualine_y = { "progress" },
+      lualine_z = { "location" },
+    },
+  },
 }
