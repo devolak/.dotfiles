c1e8edc16a123f5813e1a6de7e365ced454ae735
Tanner Kilian <tkilian12@gmail.com>
2026-02-27 00:17:14 -0700

    Added lsp support

---
+++ .config/nvimnew/lsp/lua_ls.lua
@@ -0,0 +1,13 @@
+return {
+  settings = {
+    Lua = {
+      runtime = { version = 'LuaJIT', },
+      diagnostics = {
+        globals = {
+          'vim',
+          'require',
+        },
+      },
+    },
+  },
+}
