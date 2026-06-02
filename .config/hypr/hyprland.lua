e8f43273f05dc726baed8328c9fad2ebfc429223
Tanner Kilian <tkilian12@gmail.com>
2026-06-02 16:35:37 -0700

    Swapped to qs from waybar

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -79,7 +79,7 @@ hl.on("hyprland.start", function ()
   hl.exec_cmd("xrandr --output DP-6 --primary")
   hl.exec_cmd("hyprctl dispatch 'hl.dsp.focus({ workspace = 1 })'")
 
-  hl.exec_cmd("waybar")
+  hl.exec_cmd("qs")
   hl.exec_cmd("mako")
   hl.exec_cmd("awww-daemon")
   -- hl.exec_cmd("hypridle")
